d89eb90469f3e3d918a3d5a5451da1eb8dda63e4
Sanghita Seal <sealsanghita2005@gmail.com>
2026-01-16 17:00:33 +0530

    Build Support Component

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <section className="container-fluid p-5 " id="supportHero">
+      <div className="pt-5  " id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="">Track Tickets</a>
+      </div>
+      <div className="row px-5 mr-5">
+        <div className="col-6 p-3">
+          <h1 className="fs-3">
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+          <input placeholder="Eg. how do I activate F&O" />
+          <br />
+          <a href="" className="p-4 m-2">Track account opening</a>
+          <a href=""  className="p-4">Track segment activation</a><br />
+          <a href="" className="p-4 m-2">Intraday margins</a>
+          <a href="" className="p-4">Kite user manual</a>
+        </div>
+        <div className="col-6 p-3">
+          <h1 className="fs-3">Featured</h1>
+          <ol>
+            <li>
+              <a href="">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
