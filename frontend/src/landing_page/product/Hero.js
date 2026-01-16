b8742e8cdc235195bfc062865e306e6cb04c8582
Sanghita Seal <sealsanghita2005@gmail.com>
2026-01-16 15:20:04 +0530

    Build Product Component

--- frontend/src/landing_page/product/Hero.js
+++ frontend/src/landing_page/product/Hero.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
+
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className="container m-5 p-5">
+      <div className="row p-5 ">
+        <h2 className="py-3 fs-2 text-center ">
+          Zerodha Products
+        </h2>
+        
+        <p className='fs-5 text-center'>Sleek, modern, and intuitive trading platforms<i class="fa fa-long-arrow-right" aria-hidden="true"></i></p>
+        <p className='text-center'>  Check out our {""}
+          <a href="" style={{ textDecoration: "none" }}>
+              investment offerings {""}
+              <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+            </a>
+        </p>
+      </div>
+    </div>
   )
 }
 
